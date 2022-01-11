b1835806dab2b9a4291d8124efea4ee18a9c119b
CalebKornfein <caleb.kornfein@duke.edu>
2022-01-10 19:16:40 -0500

    added MW to clustering script

--- clustering.py
+++ clustering.py
@@ -13,13 +13,15 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
 
 import warnings
+
 warnings.filterwarnings('ignore')
 
 # mapbase functions
 from mpl_toolkits.basemap import Basemap
 from pylab import rcParams
-#%matplotlib inline
-rcParams['figure.figsize'] = (14,10)
+
+# %matplotlib inline
+rcParams['figure.figsize'] = (14, 10)
 
 # plotting package
 import matplotlib.patches as mpatches
@@ -31,25 +33,26 @@ import math
 from typing import Tuple
 from CONSTANTS import CLUSTERING
 
-def apply_clustering(input_filepath:Path,
-                     output_filepath:Path,
-                     region: str, method: str='DBSCAN',
-                     save_file = False) -> pd.DataFrame:
+
+def apply_clustering(input_filepath: Path,
+                     output_filepath: Path,
+                     region: str, method: str = 'DBSCAN',
+                     save_file=True) -> pd.DataFrame:
     """
     Takes as input a csv file with columns name, lon, lat and region:
     Arguments:
      input_filepath: path to input csv
      output_filepath: directory path to store clustered data
-     region: geographical area must be one of [NE, NW, EM, SW] or None to apply
+     region: geographical area must be one of [NE, NW, EM, SW, MW] or None to apply
         clustering methods across all dataset points
      clustering_method: unsupervised ML method.
     Returns:
      csv file with clustered data, identified in the 'cluster' added column
     """
-    #download csv file
+    # download csv file
     df = pd.read_csv(input_filepath)
 
-    if region in ['NE', 'NW', 'EM', 'SW']:
+    if region in ['NE', 'NW', 'EM', 'SW', 'MW']:
         print(f'clustering {region} region')
         df = df.loc[df.region == region]
 
@@ -57,7 +60,7 @@ def apply_clustering(input_filepath:Path,
         print(f'clustering across all regions')
 
     else:
-        print(f'{region} is an invalid region [NE, NW, EM, SW]')
+        print(f'{region} is an invalid region [NE, NW, EM, SW, MW]')
         return
 
     df_lon_lat = df[["lon", "lat"]]
@@ -71,11 +74,11 @@ def apply_clustering(input_filepath:Path,
 
     cluster = cluster_function(**params).fit(df_scaled)
     labels = cluster.labels_
-    df["cluster"]=labels
+    df["cluster"] = labels
 
     # check # of clusters excluding noise (-1), if cluster method produce noise
     num_labels = set(labels)
-    realClusterNum=len(num_labels) - (1 if -1 in labels else 0)
+    realClusterNum = len(num_labels) - (1 if -1 in labels else 0)
     clusterNum = len(num_labels)
     print(f'There are {realClusterNum} clusters, excluding noise')
 
@@ -89,35 +92,33 @@ def apply_clustering(input_filepath:Path,
     return df
 
 
-def plot_clusters(input_filepath:Path,
-                  output_filepath:Path, region: str='NE'):
+def plot_clusters(input_filepath: Path,
+                  output_filepath: Path, region: str = 'MW'):
     """
     Takes as input a csv file produced by the apply_clustering function:
     Arguments:
        output_filepath: path to store png file
-       region: geographical area must be one of [NE, NW or EM]
+       region: geographical area must be one of [NE, NW, EM, SW, MW]
     Returns:
        png file with plot of data clustered
     """
 
-    FILENAME = f'{region}_clusters.csv'
-    filepath = input_filepath/FILENAME
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(input_filepath)
     # set area to plot
     print(df.describe().loc[['min', 'max']])
 
     stats = df.describe()
-    llon = math.floor(stats.at['min', 'lon']-1)
-    ulon = math.ceil(stats.at['max', 'lon']+1)
-    llat = math.floor(stats.at['min', 'lat']-0.5)
-    ulat = math.ceil(stats.at['max', 'lat']+0.5)
+    llon = math.floor(stats.at['min', 'lon'] - 1)
+    ulon = math.ceil(stats.at['max', 'lon'] + 1)
+    llat = math.floor(stats.at['min', 'lat'] - 0.5)
+    ulat = math.ceil(stats.at['max', 'lat'] + 0.5)
 
     map = Basemap(projection='merc',
-                  resolution = 'l', area_thresh = 1000.0,
-                  llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
-                  urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+                  resolution='l', area_thresh=1000.0,
+                  llcrnrlon=llon, llcrnrlat=llat,  # min longitude (llcrnrlon) and latitude (llcrnrlat)
+                  urcrnrlon=ulon, urcrnrlat=ulat)  # max longitude (urcrnrlon) and latitude (urcrnrlat)
 
-    #map.drawcoastlines()
+    # map.drawcoastlines()
     map.drawcountries()
     map.drawstates(color='black')
     map.drawlsmask(land_color='orange', ocean_color='skyblue')
@@ -130,39 +131,41 @@ def plot_clusters(input_filepath:Path,
     colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, clusterNum))
 
     # collect data based on wind turbines
-    xs,ys = map(np.asarray(df.lon), np.asarray(df.lat))
-    df['xm']= xs.tolist()
-    df['ym'] =ys.tolist()
+    xs, ys = map(np.asarray(df.lon), np.asarray(df.lat))
+    df['xm'] = xs.tolist()
+    df['ym'] = ys.tolist()
 
-    #Visualization2
+    # Visualization2
     for clust_number in labels:
-        c=(([0.4,0.4,0.4]) if clust_number == -1 else colors[np.int(clust_number)])
+        c = (([0.4, 0.4, 0.4]) if clust_number == -1 else colors[np.int(clust_number)])
         clust_set = df[df.cluster == clust_number]
         print(f'cluster {clust_number} has {len(clust_set)} records')
-        map.scatter(clust_set.xm, clust_set.ym, color=c,  marker='o',
-                    s= 40, alpha = 0.65)
+        map.scatter(clust_set.xm, clust_set.ym, color=c, marker='o',
+                    s=40, alpha=0.65)
         if clust_number != -1:
-            cenx=np.mean(clust_set.xm)
-            ceny=np.mean(clust_set.ym)
-            plt.text(cenx,ceny,str(clust_number), fontsize=30, color='red',
+            cenx = np.mean(clust_set.xm)
+            ceny = np.mean(clust_set.ym)
+            plt.text(cenx, ceny, str(clust_number), fontsize=30, color='red',
                      fontweight='bold')
-            #print ("Cluster "+str(clust_number))
+            # print ("Cluster "+str(clust_number))
     plt.title(f'Clusters of collected images for wind turbines in the {region} '
               f'region', fontsize=14)
-    plt.savefig(os.path.join(output_filepath,f'{region}_cluster.png'), dpi=300)
+    plt.savefig(os.path.join(output_filepath, f'{region}_cluster.png'), dpi=300)
     return
 
 
-def stratified_split(df:pd.DataFrame,
+def stratified_split(df: pd.DataFrame,
+                     output_filepath: str,
+                     region: str,
                      strata_column: str,
                      train_size: int, test_size: int,
                      n_splits: int = 5,
                      random_state: int = 42) -> Tuple:
-
     """
     Takes as input a dataframe:
     Arguments:
      df: input dataframe
+     output_filepath: directory path to store stratified train and test data
      strata_column: column used as reference for stratification
      n_splits, train_size, test_size and random_state are arguments of the
      StratifiedShuffleSplit function from scikit-learn.
@@ -174,13 +177,13 @@ def stratified_split(df:pd.DataFrame,
     labels = set(df[strata_column])
 
     if -1 in labels:
-        #drop noise in clusters
+        # drop noise in clusters
         df = df[df[strata_column] != -1]
 
-    sss = StratifiedShuffleSplit(n_splits = n_splits,
-                                 train_size = train_size,
-                                 test_size = test_size,
-                                 random_state = random_state)
+    sss = StratifiedShuffleSplit(n_splits=n_splits,
+                                 train_size=train_size,
+                                 test_size=test_size,
+                                 random_state=random_state)
 
     try:
         for train_idx, test_idx in sss.split(df, df[strata_column]):
@@ -201,5 +204,8 @@ def stratified_split(df:pd.DataFrame,
 
     print('test data cluster distribution:')
     print(test_set[strata_column].value_counts(normalize=True))
+
+    train_set.to_csv(os.path.join(output_filepath, f'{region}_train_coord.csv'), index=False)
+    test_set.to_csv(os.path.join(output_filepath, f'{region}_test_coord.csv'), index=False)
 
     return train_set, test_set
